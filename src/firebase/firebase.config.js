74377736c4172d6495e74ed0776a7bc33ddfccc4
sajjadsadiq <sajjadsadiq2020@gmail.com>
2024-04-29 23:33:14 +0600

    firebase config .env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,20 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChSjzWKKNzJvZZqyS7M0IDO1uDDHwcXgE",
-  authDomain: "gotourhero.firebaseapp.com",
-  projectId: "gotourhero",
-  storageBucket: "gotourhero.appspot.com",
-  messagingSenderId: "1092875328798",
-  appId: "1:1092875328798:web:3084c5d8b4bd6ac772fd0f",
-  measurementId: "G-VD5DTGVNL8",
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
